5b65425caec49167f863aa894147003bc9946836
Sherryll Elliott <sherryll@breakroom.cc>
2024-01-17 17:54:39 +0100

    adds coin calculator and functions to round

--- lib/match_MVP_web/live/checkout/checkout_live.ex
+++ lib/match_MVP_web/live/checkout/checkout_live.ex
@@ -8,12 +8,19 @@ defmodule Match_MVPWeb.CheckoutLive do
   def mount(_params, session, socket) do
     user_order = Orders.get_order_by_user_id(socket.assigns.current_user.id)
     change_due = Float.round(socket.assigns.current_user.deposit - user_order.total_cost, 2)
+    change_in_cents = change_due * 100
+    [change_available, leftover] = round_cash(change_in_cents)
+    coin_values = get_change(change_available)
+
 
     socket =
       socket
       |> assign_current_user(session)
       |> assign(:order, user_order)
       |> assign(:change_due, change_due)
+      |> assign(:change_available, change_available/100)
+      |> assign(:change_leftover, leftover/100)
+      |> assign(:coin_values, coin_values)
 
     {:ok, socket}
   end
@@ -31,7 +38,13 @@ defmodule Match_MVPWeb.CheckoutLive do
   end
 
   def handle_event("make_change", _params, socket) do
-    IO.puts("CHANGE BUTTON WORKS")
+    case Accounts.get_user!(socket.assigns.current_user.id) do
+      %User{} = user ->
+        Accounts.update_user(user, %{deposit: 0})
+        {:noreply, socket}
+
+      _ -> IO.puts("NO USER")
+    end
     {:noreply, socket}
   end
 
@@ -46,7 +59,37 @@ defmodule Match_MVPWeb.CheckoutLive do
     _ -> IO.puts("NO USER")
     {:noreply, socket}
     end
-
-
   end
+
+  def get_change(amount) do
+    coin_values = [100, 50, 20, 10, 5]
+    coins = coins(coin_values, amount)
+    "Euro: #{Enum.at(coins, 0)}, "<>
+    "Fifty cents: #{Enum.at(coins, 1)}, "<>
+    "Twenty cents: #{Enum.at(coins, 2)}, "<>
+    "ten cents: #{Enum.at(coins, 3)}, "<>
+    "five cents: #{Enum.at(coins, 4)}, "
+  end
+
+  def coins([], 0), do: []
+
+  def coins([coin | incoming_coins], amount) when amount < coin do
+    number_of_coin = 0
+    [number_of_coin | coins(incoming_coins, amount)]
+  end
+
+  def coins([coin | incoming_coins], amount) do
+    number_of_coin = div(amount, coin)
+    remaining_amount = rem(amount, coin)
+    [number_of_coin | coins(incoming_coins, remaining_amount)]
+  end
+
+
+  def round_cash(change_due) do
+    result = change_due / 5
+    quotient = result |> Kernel.floor
+    remainder = change_due - (quotient * 5)
+    [quotient * 5, remainder * 100]
+  end
+
 end
