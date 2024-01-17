543b546ba31e63b64bacc3ece56eb772abe280f2
Sherryll Elliott <sherryll@breakroom.cc>
2024-01-17 10:34:41 +0100

    adds orders table migration

---
+++ priv/repo/migrations/20240117092710_add_orders.exs
@@ -0,0 +1,17 @@
+defmodule Match_MVP.Repo.Migrations.AddOrders do
+  use Ecto.Migration
+
+  def change do
+    create table(:orders) do
+      add :basket, {:array, :map}, null: false
+      add :total_cost, :float, null: false
+      add :change_due, :string
+      add :user_id, references(:users, on_delete: :delete_all), null: false
+
+      timestamps()
+    end
+
+    create index(:orders, [:user_id])
+    create unique_index(:orders, [:id])
+  end
+end
